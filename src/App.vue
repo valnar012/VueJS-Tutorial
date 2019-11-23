91c3007b176648477693e6d8b194f530af32a577
Max Nussbaum <valnar012@gmail.com>
2019-11-23 15:12:02 +0100

    Progession

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,24 @@
     <p v-html="finishedLink"></p>
     <hr />
     <button v-on:click="incrementCounter($event,2)">Click me</button>
-    <p>{{counter}}</p>
+    <button v-on:click="counter++">Click me</button>
+    <p>{{counter * 2 > 10 ? 'Greater than 10' : 'Smaller or equal than 10'}}</p>
     <p v-on:mousemove="setCoordinates">
       Coordinates: {{x}} / {{y}}
       -
       <span v-on:mousemove.stop>DEAD SPOT</span>
     </p>
     <input type="text" v-on:keyup.enter="alertMe" />
+    <hr />
+    <input type="text" v-model="name" />
+    <p>{{name}}</p>
+    <hr />
+    <p>Counter: {{counter2}}</p>
+    <p>Result: {{output}}</p>
+    <button v-on:click="counter2--">Decrease</button>
+    <hr />
+    <button @click="changeLink">Click to change link</button>
+    <a :href="link2">Link</a>
   </div>
 </template>
 
@@ -25,13 +36,33 @@ export default {
     return {
       title: "Hello world",
       link: "https://vuejs.org",
+      link2: "https://vuejs.org",
       finishedLink: '<a href="https://vuejs.org"> VueJS</a>',
+      name: "Joe",
       counter: 0,
+      counter2: 0,
       x: 0,
       y: 0
     };
   },
+  computed: {
+    // Computed only gets executed when something changes
+    output() {
+      return this.counter2 > 5 ? "Greater than 5" : "Smaller or equal than 5";
+    }
+  },
+  watch: {
+    //Executes code upon data changes (Best practice)
+    //Must run in sync
+    counter2() {
+      var vm = this;
+      setTimeout(function() {
+        vm.counter2 = 0;
+      }, 10000);
+    }
+  },
   methods: {
+    // Methods gets called everytime when dom gets updated
     showTitle(data) {
       this.title = data;
       return this.title;
@@ -45,6 +76,9 @@ export default {
     },
     alertMe() {
       alert("Why are you doing this?");
+    },
+    changeLink() {
+      this.link2 = "http://google.com";
     }
   }
 };
