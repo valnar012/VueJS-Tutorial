2e143fa0189073f6d21213e4bfae27eb5ff617bd
Max Nussbaum <valnar012@gmail.com>
2019-11-23 13:45:55 +0100

    structure completed

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,34 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png" />
+    <h1 v-once>{{ title }}</h1>
+    <a v-bind:href="link">
+      <img src="./assets/logo.png" />
+    </a>
+    {{ showTitle()}}
+    <hr />
+    <p v-html="finishedLink"></p>
   </div>
 </template>
 
 <script>
 export default {
-  name: "app"
+  data() {
+    return {
+      title: "Hello world",
+      link: "https://vuejs.org",
+      finishedLink: '<a href="https://vuejs.org"> VueJS</a>'
+    };
+  },
+  methods: {
+    showTitle() {
+      this.title = "Hello";
+      return this.title;
+    }
+  }
 };
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 
 new Vue({
+  methods: {
+    title: function () {
+      return this.title;
+    }
+  },
   render: h => h(App),
 }).$mount('#app')
