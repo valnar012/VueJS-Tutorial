9efcb1b1e368af75fd99b7521437343d24e02e56
Max Nussbaum <valnar012@gmail.com>
2019-11-23 15:26:53 +0100

    Exercise done

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,19 @@
 <template>
   <div id="app">
-    <h1 v-once>{{ title }}</h1>
-    <a v-bind:href="link">
-      <img src="./assets/logo.png" />
-    </a>
-    <div>{{ showTitle("Hello")}}</div>
-    <hr />
-    <p v-html="finishedLink"></p>
-    <hr />
-    <button v-on:click="incrementCounter($event,2)">Click me</button>
-    <button v-on:click="counter++">Click me</button>
-    <p>{{counter * 2 > 10 ? 'Greater than 10' : 'Smaller or equal than 10'}}</p>
-    <p v-on:mousemove="setCoordinates">
-      Coordinates: {{x}} / {{y}}
-      -
-      <span v-on:mousemove.stop>DEAD SPOT</span>
-    </p>
-    <input type="text" v-on:keyup.enter="alertMe" />
-    <hr />
-    <input type="text" v-model="name" />
-    <p>{{name}}</p>
-    <hr />
-    <p>Counter: {{counter2}}</p>
-    <p>Result: {{output}}</p>
-    <button v-on:click="counter2--">Decrease</button>
-    <hr />
-    <button @click="changeLink">Click to change link</button>
-    <a :href="link2">Link</a>
+    <div id="exercise">
+      <!-- 1) Show a "result" of 'not there yet' as long as "value" is not equal to 37 - you can change "value" with the buttons. Print 'done' once you did it -->
+      <div>
+        <p>Current Value: {{ value }}</p>
+        <button @click="value += 5">Add 5</button>
+        <button @click="value += 1">Add 1</button>
+        <p>{{ result }}</p>
+      </div>
+      <!-- 2) Watch for changes in the "result" and reset the "value" after 5 seconds (hint: setTimeout(..., 5000) -->
+      <div>
+        <input @keydown.enter="setResetTime" :value="resetTime" type="text" />
+        <p>{{ value }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -34,51 +21,26 @@
 export default {
   data() {
     return {
-      title: "Hello world",
-      link: "https://vuejs.org",
-      link2: "https://vuejs.org",
-      finishedLink: '<a href="https://vuejs.org"> VueJS</a>',
-      name: "Joe",
-      counter: 0,
-      counter2: 0,
-      x: 0,
-      y: 0
+      value: 0,
+      resetTime: 5000
     };
   },
   computed: {
-    // Computed only gets executed when something changes
-    output() {
-      return this.counter2 > 5 ? "Greater than 5" : "Smaller or equal than 5";
+    result() {
+      return this.value >= 37 ? "done" : "";
     }
   },
   watch: {
-    //Executes code upon data changes (Best practice)
-    //Must run in sync
-    counter2() {
+    result() {
       var vm = this;
       setTimeout(function() {
-        vm.counter2 = 0;
-      }, 10000);
+        vm.value = 0;
+      }, this.resetTime);
     }
   },
   methods: {
-    // Methods gets called everytime when dom gets updated
-    showTitle(data) {
-      this.title = data;
-      return this.title;
-    },
-    incrementCounter(event, step = 1) {
-      this.counter += step;
-    },
-    setCoordinates: function(event) {
-      this.x = event.clientX;
-      this.y = event.clientY;
-    },
-    alertMe() {
-      alert("Why are you doing this?");
-    },
-    changeLink() {
-      this.link2 = "http://google.com";
+    setResetTime(event) {
+      this.resetTime = event.target.value;
     }
   }
 };
